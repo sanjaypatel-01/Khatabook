18c14aec4dbe2b7802c54d91c65d0a55d43b133a
Sanjay Patel <sanjaypatel5346@gmail.com>
2024-12-22 18:48:01 +0530

    added create, edit functionality

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -18,6 +19,32 @@ app.get("/", (req, res) =>  {
 
 app.get("/create", function (req, res) {
     res.render("create");
+});
+
+app.get("/edit/:filename", function (req, res) {
+    fs.readFile(`./hisaab/${req.params.filename}`, "utf-8", function(err, filedata) {
+        if(err) return res.status(500).send(err);
+        res.render("edit", {filedata, filename: req.params.filename});
+    })
+});
+
+app.post("/update/:filename", function (req, res) {
+    fs.writeFile(`./hisaab/${req.params.filename}`, req.body.content, function(err){
+        if(err) return res.status(500).send(err);
+        res.redirect("/");
+    })
+});
+
+app.post("/createhisaab", function (req, res) {
+    // res.send(req.body);
+
+    var currentDate = new Date(); 
+    var date = `${currentDate.getDate()}-${currentDate.getMonth()+1}-${currentDate.getFullYear()}`;  //month pe +1 ishliye kia, kyunki ye month ko 0 se count krte hein
+
+    fs.writeFile(`./hisaab/${date}.txt`, req.body.content, function(err){ //./hisaab/${req.body.title} ab hame title ish se nahi cahiye
+        if(err) return res.status(500).send(err);
+        res.redirect("/");   
+    });
 });
 
 app.listen(3000); 
